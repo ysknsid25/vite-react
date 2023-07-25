24b06bc1a0867fa8595ab6e93fd6f33fc6952d8b
ysknsid25 <kengo071225@gmail.com>
2023-07-25 09:17:21 +0900

    時間のmockテスト
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,38 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import App from '../App';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
-test('renders h1 text', () => {
-    render(<App />);
-    const headerElement = screen.getByText("Vite + React");
-    expect(headerElement).toBeInTheDocument();
-});
+const sut = {
+    purchase: () => {
+        const businessHours = [9, 17]
+        const currentHour = new Date().getHours()
+        const [open, close] = businessHours
+
+        if (currentHour > open && currentHour < close)
+            return { message: 'Success' }
+
+        return { message: 'Error' }
+    }
+}
+
+describe(`購入フローのテスト`, () => {
+    beforeEach(() => {
+        // Mockを使う
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        // Mockではなくリアルタイムを使う
+        vi.useRealTimers()
+    })
+
+    it(`営業時間内の場合、purchase()はErrorを返す`, () => {
+        const date = new Date(2000, 1, 1, 13)
+        vi.setSystemTime(date)
+        expect(sut.purchase()).toEqual({ message: 'Success' })
+    })
+
+    it(`営業時間外の場合、purchase()はErrorを返す`, () => {
+        const date = new Date(2000, 1, 1, 19)
+        vi.setSystemTime(date)
+        expect(sut.purchase()).toEqual({ message: 'Error' })
+    })
+})
